c3d6ebfb25fec2fd464ec7f7a1ceee6ebe87c68f
Yunus AbdulQudus <yunusabdulqudus1@gmail.com>
2023-10-24 15:12:29 +0100

    activated round blue button on workspace empty

--- src/components/canvas/workspace/CanvasWorkspaceEmpty.vue
+++ src/components/canvas/workspace/CanvasWorkspaceEmpty.vue
@@ -4,17 +4,28 @@
       Hit the <span>+</span> button to <span>drag and drop</span> components
       from the component panel
     </p>
-    <button>
+    <button @click="openSidebar">
       <BaseIcon icon="add" />
     </button>
   </div>
 </template>
-<script>
+<script lang="ts">
 import { defineComponent } from "vue";
-import BaseIcon from "../../icon/BaseIcon";
+import BaseIcon from "../../icon/BaseIcon.vue";
+import store from "@/store";
 
 export default defineComponent({
   name: "CanvasWorkspaceEmpty",
   components: { BaseIcon },
+
+  setup() {
+    const openSidebar = () => {
+      store.commit("canvas/SET_SIDEBAR_NAVBAR_CONTENT", "add_component");
+    };
+
+    return {
+      openSidebar,
+    };
+  },
 });
 </script>

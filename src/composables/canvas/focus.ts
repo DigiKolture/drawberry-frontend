aabc13de0bacc9d5212af0294c3534f1c4a7776a
AbdulQudus Yunus <yunusabdulqudus1@gmail.com>
2024-01-04 10:25:34 +0100

    Fix panel tab styles

--- src/composables/canvas/focus.ts
+++ src/composables/canvas/focus.ts
@@ -115,11 +115,12 @@ export function focus() {
 
     const focusedElement = componentItem.json[jsonIndex];
     store.commit("canvas/SET_FOCUSED_ELEMENT", focusedElement);
+    store.commit("canvas/SET_FOCUSED_INDEX", itemIndex);
 
     setParentFocusedElement(focusedElement, componentItem);
 
-    store.commit("canvas/SET_FOCUSED_INDEX", itemIndex);
     store.commit("layers/SET_ACTIVE_TAB_STATE", itemIndex);
+
     store.commit("panel/RESET_TAB_STATES");
     store.commit("panel/ACTIVATE_FIRST_TAB_STATE");
 

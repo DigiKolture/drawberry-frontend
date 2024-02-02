c0cd2f03cf6f2b3583f49e74de8c00f8a8207cf1
AbdulQudus Yunus <yunusabdulqudus1@gmail.com>
2024-02-02 15:51:40 +0100

    Created base component for image upload

---
+++ src/components/canvas/panel/BaseImageTextUpload.vue
@@ -0,0 +1,90 @@
+<template>
+  <PanelStyleTabs @update="updateTab" :titles="titles">
+    <BaseMediaImageUpload
+      v-model="src"
+      @update="updateImage"
+      v-if="activeIndex === 0"
+    />
+    <div v-if="activeIndex === 1" class="content__style__media__text">
+      <input
+        v-model="src"
+        type="url"
+        @focus="isInputFocused = true"
+        @blur="isInputFocused = false"
+        required
+        class="input__style__text"
+        placeholder="Enter Url"
+      />
+      <BaseButtonIcon
+        v-if="isInputFocused"
+        @mousedown.prevent="updateImage"
+        icon="canvas/panel/styles/media/update"
+      />
+      <BaseButtonIcon
+        v-if="!isInputFocused && src"
+        @click="clearLink"
+        icon="canvas/panel/styles/media/cancel"
+      />
+    </div>
+  </PanelStyleTabs>
+</template>
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "vue";
+import BaseMediaImageUpload from "@/components/canvas/panel/BaseMediaImageUpload.vue";
+import PanelStyleTabs from "@/components/canvas/panel/PanelStyleTabs.vue";
+import BaseButtonIcon from "@/components/icon/BaseButtonIcon.vue";
+import { helpers } from "@/composables/helpers";
+
+export default defineComponent({
+  name: "BaseImageTextUpload",
+  components: {
+    BaseButtonIcon,
+    PanelStyleTabs,
+    BaseMediaImageUpload,
+  },
+  props: {
+    modelValue: {
+      type: String,
+      default: "",
+    },
+  },
+  setup(props, { emit }) {
+    const titles = ["Upload", "Url"];
+    const { isValidImageUrl } = helpers();
+
+    const src = ref("");
+    let activeIndex = ref(0);
+    const isInputFocused = ref(false);
+
+    onMounted(() => {
+      src.value = props.modelValue;
+    });
+
+    const updateTab = (index: number) => {
+      activeIndex.value = index;
+    };
+
+    const clearLink = async () => {
+      src.value = "";
+    };
+
+    const updateImage = async () => {
+      const isValid = await isValidImageUrl(src.value);
+      if (!isValid && src.value) return;
+      emit("update:modelValue", src.value);
+      emit("confirm", src.value);
+      isInputFocused.value = false;
+    };
+
+    return {
+      src,
+      titles,
+      activeIndex,
+      isInputFocused,
+      updateTab,
+      updateImage,
+      clearLink,
+    };
+  },
+});
+</script>

fc85a00e1d8cf2900fea1bc3cb0434d0c7b86b05
AbdulQudus Yunus <yunusabdulqudus1@gmail.com>
2024-03-21 00:39:19 +0100

    Added projects skeleton

---
+++ src/components/projects/ProjectsSkeleton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="projects__skeletons">
+    <div v-for="i in 6" :key="i" class="projects__skeletons__item">
+      <div class="projects__skeletons__item__img"></div>
+      <div class="projects__skeletons__item__title"></div>
+      <div class="projects__skeletons__item__desc"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ProjectsSkeleton",
+});
+</script>
+
+<style></style>
--- src/views/Projects/ProjectIndex.vue
+++ src/views/Projects/ProjectIndex.vue
@@ -1,9 +1,12 @@
 <template>
   <AppLayout :title="title" :description="description">
     <div class="projects__container">
-      <ProjectsEmpty v-if="projects.length === 0" />
+      <ProjectsSkeleton v-if="loading" />
 
-      <ProjectsSection />
+      <template v-else>
+        <ProjectsEmpty v-if="projects.length === 0" />
+        <ProjectsSection />
+      </template>
     </div>
     <div class="view__bg">
       <router-view @close="close" />
@@ -11,29 +14,36 @@
   </AppLayout>
 </template>
 <script>
-import { computed, defineComponent, onMounted } from "vue";
+import { computed, defineComponent, onMounted, ref } from "vue";
 import AppLayout from "@/components/layout/AppLayout";
 import store from "@/store";
 import router from "@/router";
 import ProjectsEmpty from "@/components/projects/ProjectsEmpty";
 import ProjectsSection from "@/components/projects/ProjectsSection";
+import ProjectsSkeleton from "@/components/projects/ProjectsSkeleton.vue";
 
 export default defineComponent({
   name: "ProjectIndex",
-  components: { ProjectsSection, ProjectsEmpty, AppLayout },
+  components: { ProjectsSkeleton, ProjectsSection, ProjectsEmpty, AppLayout },
 
   setup() {
     const title = "All Projects";
     const description =
       "Manage your projects, create or add projects to folders.";
 
+    const loading = ref(false);
+
     const projects = computed(() => {
       return store.getters["projects/projects"];
     });
 
-    onMounted(() => {
-      store.dispatch("projects/getProjects");
-      store.dispatch("folders/getFolders");
+    onMounted(async () => {
+      loading.value = true;
+      await Promise.all([
+        store.dispatch("projects/getProjects"),
+        store.dispatch("folders/getFolders"),
+      ]);
+      loading.value = false;
     });
 
     const close = () => {
@@ -42,6 +52,7 @@ export default defineComponent({
 
     return {
       title,
+      loading,
       description,
       close,
       projects,

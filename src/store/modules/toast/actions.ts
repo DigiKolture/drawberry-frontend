c79dfffea61385159ed722b596ba91f14e340c56
AbdulQudus Yunus <yunusabdulqudus1@gmail.com>
2024-05-08 23:55:41 +0100

    Update timeout

--- src/store/modules/toast/actions.ts
+++ src/store/modules/toast/actions.ts
@@ -11,8 +11,10 @@ const dataDefault = {
 export const actions: ActionTree<ToastState, RootState> = {
   showToast({ commit }, { message, type = "success", data = dataDefault }) {
     commit("SHOW_TOAST", { message, type, data });
+    let timeout = 5000;
+    if (data.action) timeout = 8000;
     setTimeout(() => {
       commit("HIDE_TOAST");
-    }, 5000);
+    }, timeout);
   },
 };

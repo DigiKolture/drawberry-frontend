ca14fbaa787235de025ae0a324458f2bebc6b9d5
AbdulQudus Yunus <yunusabdulqudus1@gmail.com>
2024-05-31 11:28:17 +0100

    Fixed component sidebar

--- src/components/canvas/sidebar/components/ComponentsContainerListItem.vue
+++ src/components/canvas/sidebar/components/ComponentsContainerListItem.vue
@@ -7,12 +7,13 @@
         active: selectedComponent && selectedComponent.id === component.id,
       }"
       href="#"
-      >{{ component.name }}</a
     >
+      {{ component.name }}
+    </a>
   </li>
 </template>
 <script>
-import { defineComponent, ref } from "vue";
+import { computed, defineComponent, onMounted, ref } from "vue";
 import store from "@/store";
 import { ui } from "@/assets/js/canvas";
 
@@ -26,14 +27,17 @@ export default defineComponent({
   },
 
   setup() {
-    const selectedComponent = ref(() => {
+    const selectedComponent = computed(() => {
       return store.getters["components/selectedComponent"];
+    });
+
+    onMounted(() => {
+      store.commit("components/SET_SELECTED_COMPONENT", null);
     });
 
     const fetchComponentItems = (component) => {
       ui.changeComponentItemsStatus();
-      selectedComponent.value = component;
-      store.commit("components/SET_SELECTED_COMPONENT", selectedComponent);
+      store.commit("components/SET_SELECTED_COMPONENT", component);
       store.dispatch("components/getComponentItems", component.id);
     };
 
